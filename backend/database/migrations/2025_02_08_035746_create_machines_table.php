fc6db66b1ca528e9ce36dd378406738df631acce
ArunaUsitha <arunausitha.info@gmail.com>
2025-02-08 09:30:54 +0530

    initialize machine model

---
+++ backend/database/migrations/2025_02_08_035746_create_machines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('machines', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('model');
+            $table->string('brand');
+            $table->date('purchase_date');
+            $table->decimal('purchase_price', 10, 2);
+            $table->string('category');
+            $table->integer('reset_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('machines');
+    }
+};
