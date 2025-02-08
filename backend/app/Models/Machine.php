8af9520eab99064f915263cb9b695b8fb932f62d
ArunaUsitha <arunausitha.info@gmail.com>
2025-02-08 09:49:24 +0530

    added model relations

--- backend/app/Models/Machine.php
+++ backend/app/Models/Machine.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Machine extends Model
 {
-    //
+    public function resets(): HasMany
+    {
+        return $this->hasMany(MachineReset::class);
+    }
+
+    public function operations(): HasMany
+    {
+        return $this->hasMany(MachineOperation::class);
+    }
 }
--- backend/app/Models/MachineReset.php
+++ backend/app/Models/MachineReset.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MachineReset extends Model
 {
-    //
+    public function machine(): BelongsTo
+    {
+        return $this->belongsTo(Machine::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
