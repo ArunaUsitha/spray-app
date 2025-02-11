427088e473a9bbcec7042e31bfe62e6982b7c780
ArunaUsitha <arunausitha.info@gmail.com>
2025-02-11 14:28:17 +0530

    code cleanup

--- frontend/src/pages/Machines/Machine.vue
+++ frontend/src/pages/Machines/Machine.vue
@@ -212,7 +212,7 @@ function submit() {
           <select v-model="selectedBrand" @change="updateModels" id="brand" name="brand"
                   class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
             <option value="" disabled selected>Select a brand</option>
-            <option v-for="(brand) in sprayerBrands" :key="brand" :value="brand">{{ brand }}</option>
+            <option v-for="brand in Object.keys(sprayerBrands)" :key="brand" :value="brand">{{ brand }}</option>
           </select>
         </div>
 
